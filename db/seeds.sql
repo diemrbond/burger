ec619aa6486d342e15ecd4aaea58c955d202d324
Andrew <andrew@cell-media.com>
2020-11-20 10:43:37 +0800

    Commencing basic setup

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Peach, Mushroom & Mayo Burger');
+INSERT INTO burgers (burger_name) VALUES ('Spicy Chicken Burger');
+INSERT INTO burgers (burger_name) VALUES ('Double Quarter Pounder Burger');
---
+++ server.js
@@ -0,0 +1,7 @@
+// DEPENDENCIES
+var express = require("express");
+require('dotenv').config()
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
