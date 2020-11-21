5d73c4ffa3670ab7538ab1fcadf53cc6d30b29d3
Andrew <mrbondmustdie@gmail.com>
2020-11-21 11:07:09 +0800

    Fixed connection

--- config/connection.js
+++ config/connection.js
@@ -4,11 +4,13 @@ const logSymbols = require('log-symbols');
 
 // CREATE CONNECTION OBJECT
 console.log("process.env.JAWSDB_URL: "+process.env.JAWSDB_URL);
+const connection;
+
 if (process.env.JAWSDB_URL){
-  const connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
 else {
-  const connection = mysql.createConnection({
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
